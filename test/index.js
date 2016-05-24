6e68fc559bc8d18d173e72a29a6ccd5507f3d299
Autarc <autarc@gmail.com>
2016-05-24 19:15:06 +0100

    extend test

--- test/index.js
+++ test/index.js
@@ -9,9 +9,12 @@ var isPseudo = require('../lib/index').default
 const examples = [
   'div:hover',
   '-ms-clear',
-  'false'
+  'false',
+  ':has',
+  'nth-last-last-child: :last-child',
+  'nth-last-last-child: :nth-last-last-child(1)'
 ]
 
 examples.forEach(function (example) {
-  console.log(example, isPseudo(example))
+  console.log(example, isPseudo(example, true))
 })
